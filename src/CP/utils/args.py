6cb3758bc6de1d3dc05c83de993a12a05deb52b0
Matteo Conti <contimatteo1505@gmail.com>
2022-05-31 16:27:49 +0200

    refactoring: restart analyzing the code from scratch

--- src/CP/utils/args.py
+++ src/CP/utils/args.py
@@ -4,11 +4,11 @@ from .storage import CP_data_file_url
 
 ###
 
-MODELS_CHOICES = ["v5", "v6"]
+MODELS_CHOICES = ["base"]
 SOLVERS_CHOICES = ["Gecode", "Chuffed"]
 OUTPUT_CHOICES = ["raw", "plot", "raw+plot"]
 
-DEFAULT_MS_TIME_LIMIT = 60 * 1000
+DEFAULT_MS_TIME_LIMIT = 10 * 1000
 DEFAULT_MODEL_NAME = "v6"
 DEFAULT_SOLVER_NAME = "Gecode"
 DEFAULT_N_SOLUTIONS = 9
