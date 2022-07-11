8b277e54ddc846f8b7712d07d5fd8735a85bec67
LeonardoM999 <leonardo.monti.007@gmail.com>
2022-07-11 12:03:30 +0200

    no more python errors, missing constraint to bind x[c], y[c] to grid

--- src/SAT/models/base.py
+++ src/SAT/models/base.py
@@ -3,7 +3,7 @@ from operator import indexOf
 from z3 import *
 import numpy as np
 import math
-from components import heuristics
+from SAT.models.components import heuristics
 
 
 def at_least_one(bool_vars):
@@ -18,10 +18,11 @@ def exactly_one(bool_vars):
     return at_most_one(bool_vars) + [at_least_one(bool_vars)]
 
 
-def bool2int(l:list(Bool)):
+def bool2int(l):
     result = 0
+
     for digits in l:
-        result = (result << 1) | digits
+        result = (result << 1) | bool(str(digits)) #TODO: wtf?
     return result
 
 
@@ -36,7 +37,7 @@ def int2boolList(n:int):
     return result
 
 
-def ge(l1:list(Bool), l2:list(Bool)):
+def ge(l1, l2):
     #AND encoding: complexity n^2 -> not very good
     n = len(l1)
     first = Or(l1[0], Not(l2[0])) #l1[0] >= l2[0]
@@ -54,7 +55,7 @@ def ge(l1:list(Bool), l2:list(Bool)):
 #contains a more efficient encoding for lex
 
 
-def le(l1:list(Bool), l2:list(Bool)):
+def le(l1, l2):
     #lex_lesseq(list1, list2)
     #AND encoding: complexity n^2 -> not very good
     n = len(l1)
@@ -69,7 +70,7 @@ def le(l1:list(Bool), l2:list(Bool)):
     return And(first, rest)
 
 
-def lt(l1:list(Bool), l2:list(Bool)):
+def lt(l1, l2):
     #return And(le(l1, l2), Not(eq(l1, l2))) #inefficient
     #AND encoding: complexity n^2 -> not very good
     n = len(l1)
@@ -84,7 +85,7 @@ def lt(l1:list(Bool), l2:list(Bool)):
     return And(first, rest)
 
 
-def gt(l1:list(Bool), l2:list(Bool)):
+def gt(l1, l2):
     #return And(ge(l1, l2), Not(eq(l1, l2))) #inefficient
     #AND encoding: complexity n^2 -> not very good
     n = len(l1)
@@ -99,15 +100,15 @@ def gt(l1:list(Bool), l2:list(Bool)):
     return And(first, rest)
 
 
-def ne(l1:list(Bool), l2:list(Bool)):
+def ne(l1, l2):
     return Or([Xor(l1[bit], l2[bit]) for bit in range(len(l2))])
 
 
-def eq(l1:list(Bool), l2:list(Bool)):
+def eq(l1, l2):
     return And([Not(Xor(l1[i], l2[i])) for i in range(len(l1))])
 
 
-def lt_int(l:list(Bool), n: int):
+def lt_int(l, n):
     #provide constraint list so that bool2int(l) < n
     base2 = format(n, "b")
     if len(l) < len(base2):
@@ -131,6 +132,12 @@ def lt_int(l:list(Bool), n: int):
 
     return And(constraint_list)
 
+
+def equation(point_of_grid:list(Bool), x_c:list(Bool), w:int, y_c:list(Bool), h:int, width:int):
+    #point_of_grid = (x_c + w) + (y_c + h) * width
+    pass #TODO
+
+
 def baseSAT(data_dict: dict) -> dict:
     #data_dict = {"data":str, "width": int, "n_circuits": int, "dims":[(w,h)]}
 
@@ -150,56 +157,68 @@ def baseSAT(data_dict: dict) -> dict:
     max_makespan = heuristics.compute_max_makespan(heigths, widths, width)
             
     solver = Solver()
-
-    ### grid (linearized)
-    domain_size_grid = math.ceil(math.log2((width-min(widths))*(max_makespan-min(heigths))))
-    grid = [ [Bool(f"point{p}_{coord}") for coord in range(domain_size_grid)] for p in range(sum_area)]
-    # Each point p of the sum_area (point p of a circuit) is assigned to the coord on the board
-
-    # #int values for x coordinate of circuit c
-    # def x_int(c: int) -> int:
-    #     point_of_sum_area = sum([heigths(i)*widths(i) for i in range(c)]) #TODO correct?
-    #     bool_coord = grid(point_of_sum_area)
-    #     point_of_grid = bool2int(bool_coord)
-    #     return point_of_grid % width
-
-    # #int values for y coordinates of circuit c
-    # def y_int(c: int) -> int:
-    #     point_of_sum_area = sum([heigths(i)*widths(i) for i in range(c)])
-    #     bool_coord = grid(point_of_sum_area)
-    #     point_of_grid = bool2int(bool_coord)
-    #     return point_of_grid // width
-    
+        
     domain_size_x = math.ceil(math.log2(width - min(widths)))
     domain_size_y = math.ceil(math.log2(max_makespan - min(heigths)))
     x = [[Bool(f"x_of_c{c}_{i}") for i in range(domain_size_x)] for c in CIRCUITS]
     y = [[Bool(f"y_of_c{c}_{i}") for i in range(domain_size_y)] for c in CIRCUITS]
 
+    ### grid (linearized)
+    domain_size_grid = math.ceil(math.log2((width-min(widths))*(max_makespan-min(heigths))))
+    grid = [ [Bool(f"point{p}_{coord}") for coord in range(domain_size_grid)] for p in range(sum_area)]
+    # Each point p of the sum_area (point p of a circuit) is assigned to the coord on the board
+   
+    #int values for x coordinate of circuit c
+    # def x_c(c: int) -> int:
+    #     point_of_sum_area = sum([heigths(i)*widths(i) for i in range(c)])
+    #     bool_coord = grid(point_of_sum_area)
+    #     point_of_grid = bool2int(bool_coord)
+    #     return point_of_grid % width
+
+    # #int values for y coordinates of circuit c
+    # def y_c(c: int) -> int:
+    #     point_of_sum_area = sum([heigths(i)*widths(i) for i in range(c)])
+    #     bool_coord = grid(point_of_sum_area)
+    #     point_of_grid = bool2int(bool_coord)
+    #     return point_of_grid // width
+
+    #constraint to bind grid values to x,y
+    for c in CIRCUITS:
+        for h in range(heigths[c]):
+            for w in range(widths[c]):
+                point_of_sum_area = sum([heigths(i)*widths(i) for i in range(c)]) + widths[c]*h + w
+                solver.add(equation(grid[point_of_sum_area], x[c], w, y[c], h, width))
+                #grid[point_of_sum_area] = (x[c]+w) + (y[c]+h)*width
+
+
+
+
     #makespan is not known yet
     
     ### all circuits must have each dimension greater than zero
-    assert(min(heigths > 0 and min(widths) > 0))
+    assert(min(heigths) > 0 and min(widths) > 0)
     assert(len(heigths) == len(widths) == n_circuits)
+
+
+    #diffn: noOverlap => alldifferent(grid[])
+    solver.add(
+        And([
+            ne(grid[s1], grid[s2])
+            for s1 in range(len(grid))
+            for s2 in range(len(grid))
+            if s1<s2
+        ])
+    )
+
+    #forall(c in CIRCUITS)(x[c] + widths[c] <= width)
+    solver.add( And([lt_int(x[c], width-widths[c]) for c in CIRCUITS]) )
 
     check = sat
     while check == sat:
 
-        #diffn: noOverlap => alldifferent(grid[])
-        solver.add( #TODO check if it is faster to remove the AND and let z3 do the job
-            And([
-                ne(grid[s1], grid[s2])
-                for s1 in range(len(grid))
-                for s2 in range(len(grid))
-                if s1<s2
-            ])
-        )
-
-        #forall(c in CIRCUITS)(x[c] + widths[c] <= width)
-        solver.add( [lt_int(x[c], width-widths(c)) for c in CIRCUITS] )
-
+        solver.push()
         #forall(c in CIRCUITS)(y[c] + heights[c] <= max_makespan)
-        solver.add( [lt_int(y[c], max_makespan-heigths[c]) for c in CIRCUITS] )
-
+        solver.add( And([lt_int(y[c], max_makespan-heigths[c]) for c in CIRCUITS]) )
 
         solutions_dict = {"all_solutions": [], "solution": {}, "stats": [], "model": "base", "data": data_dict["data"], "solver": "z3 SAT"}
         # each solution in all_solutions is a dict
@@ -208,20 +227,27 @@ def baseSAT(data_dict: dict) -> dict:
         check = solver.check()
         if check == sat:
             model = solver.model()
-            y_int = [[bool2int(model.evaluate(y[c][i]))for i in range(domain_size_y)] for c in CIRCUITS]
+            y_int = [bool2int([model.evaluate(y[c][i])for i in range(domain_size_y)]) for c in CIRCUITS]
+
+            makespan = max(y_int) + heigths[indexOf(y_int, max(y_int))]
+            print(f"max_makespan:{max_makespan}  sat:{makespan}")
             solution = {"width": data_dict["width"], "n_circuits": data_dict["n_circuits"], "widths": widths, "heights": heigths, 
-                "x": [[bool2int(model.evaluate(x[c][i])) for i in range(domain_size_x)] for c in CIRCUITS], 
+                "x": [bool2int([model.evaluate(x[c][i]) for i in range(domain_size_x)]) for c in CIRCUITS], 
                 "y": y_int,
                 "min_makespan": min_makespan, "max_makespan": max_makespan,
-                "makespan": y_int +  heigths[indexOf(max(y_int))]
+                "makespan": makespan
             }
             solutions_dict["all_solutions"].append(solution)
-
-        max_makespan = (y_int +  heigths[indexOf(max(y_int))]) - 1
+            print(solution)
+            solver.pop()
+        else:
+            print("unsat")
+        max_makespan = makespan - 1
         #it is possible to decrease max_makespan at pace > 1 and when unsat try the skipped values
 
     #while check == sat
 
+    solutions_dict["stats"] = solver.statistics()
     solutions_dict["solution"] = solutions_dict["all_solutions"][0]
     return solutions_dict
 
