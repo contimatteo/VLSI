bbd9bb8f61409e8c0d03ded20e42630b6f72e153
Matteo Conti <contimatteo1505@gmail.com>
2022-07-19 17:53:42 +0200

    minor changes

--- src/SAT/utils/args.py
+++ src/SAT/utils/args.py
@@ -11,7 +11,9 @@ DEFAULT_N_SOLUTIONS = 1
 DEFAULT_VERBOSE = 1
 DEFAULT_MODEL_NAME = "base.py"
 MODELS_CHOICES = [
-    "base", "rotation", "rotation.search", "rotation.search.symmetry", "search", "search.symmetry"
+    # "base", "rotation", "rotation.search", "rotation.search.symmetry", "search", "search.symmetry"
+    "base",
+    "base_decimal_encoding_diffn"
 ]
 
 ###
