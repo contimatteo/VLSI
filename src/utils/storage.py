f14fbf1691401c76bf45dac1a7cc7ceb854bcb96
Matteo Conti <contimatteo1505@gmail.com>
2022-08-03 15:32:32 +0200

    [CP] deleted legacy files

--- src/utils/storage.py
+++ src/utils/storage.py
@@ -27,7 +27,6 @@ class _Storage:
         return self._root_dir.joinpath("solvers")
 
     def out_dir(self) -> Path:
-        # return self._root_dir.joinpath("out")
         return self.data_dir().joinpath("output/json")
 
     #
@@ -63,13 +62,6 @@ class CPStorageClass(_Storage):
     def model_file_url(self, file_name: str) -> Path:
         assert isinstance(file_name, str)
         return self.models_dir().joinpath(f"{file_name}.mzn")
-
-    # def out_file_url(self, file_name: str, sub_dir: str = None) -> Path:
-    #     assert isinstance(file_name, str)
-    #     assert sub_dir is None or isinstance(sub_dir, str)
-    #     partial_file_url = f"{sub_dir}/" if sub_dir is not None else ""
-    #     self.out_dir().joinpath(partial_file_url).mkdir(exist_ok=True, parents=True)
-    #     return self.out_dir().joinpath(f"{partial_file_url}{file_name}.json")
 
 
 class SATStorageClass(_Storage):
