3425dacf4aa9eb68d2852665e08f732ca86058de
Matteo Conti <contimatteo1505@gmail.com>
2022-08-08 19:34:32 +0200

    minor changes

--- scripts/SAT_run_all.sh
+++ scripts/SAT_run_all.sh
@@ -6,9 +6,9 @@ trap break INT
 
 instances=(
     "ins-1" "ins-2" "ins-3" "ins-4" "ins-5" "ins-6" "ins-7" "ins-8" "ins-9" "ins-10" 
-    # "ins-11" "ins-12" "ins-13" "ins-14" "ins-15" "ins-16" "ins-17" "ins-18" "ins-19" "ins-20" 
-    # "ins-21" "ins-22" "ins-23" "ins-24" "ins-25" "ins-26" "ins-27" "ins-28" "ins-29" "ins-30" 
-    # "ins-31" "ins-32" "ins-33" "ins-34" "ins-35" "ins-36" "ins-37" "ins-38" "ins-39" "ins-40"
+    "ins-11" "ins-12" "ins-13" "ins-14" "ins-15" "ins-16" "ins-17" "ins-18" "ins-19" "ins-20" 
+    "ins-21" "ins-22" "ins-23" "ins-24" "ins-25" "ins-26" "ins-27" "ins-28" "ins-29" "ins-30" 
+    "ins-31" "ins-32" "ins-33" "ins-34" "ins-35" "ins-36" "ins-37" "ins-38" "ins-39" "ins-40"
 )
 
 #
