b5a756eceede099c6c92e3480e2111ae1a6c4909
DavideSangiorgi <davide.sangiorgi@outlook.com>
2022-08-20 09:38:34 +0200

    jsonToCsv.py: added SAT lines

--- CSV/jsonToCsv.py
+++ CSV/jsonToCsv.py
@@ -1,25 +1,43 @@
 import os
 import glob
 import pandas as pd
+from tqdm import tqdm
 
 out_folders = [
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "chuffed",
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep +
-    "chuffed",
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "rotation.search" + os.sep +
-    "chuffed",
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "search" + os.sep +
-    "chuffed",
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "search.symmetry" + os.sep +
-    "chuffed",
-    "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "symmetry" + os.sep +
-    "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep +
+    # "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "rotation.search" + os.sep +
+    # "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "search" + os.sep +
+    # "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "search.symmetry" + os.sep +
+    # "chuffed",
+    # "src" + os.sep + "CP" + os.sep + "out" + os.sep + "json" + os.sep + "symmetry" + os.sep +
+    # "chuffed",
+
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "binary",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "binary.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "binary.symmetry",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "binary.symmetry.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear.symmetry",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear.symmetry.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "binary",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "binary.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "binary.symmetry",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "binary.symmetry.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "linear",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "linear.cumulative",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "linear.symmetry",
+    "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "rotation" + os.sep + "linear.symmetry.cumulative",
+
     # "src" + os.sep + "ILP" + os.sep + "out" + os.sep + "json" + os.sep + "base",
-    # "src" + os.sep + "SAT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear",
     # "src" + os.sep + "SMT" + os.sep + "out" + os.sep + "json" + os.sep + "base" + os.sep + "linear"
 ]
 
-for folder in out_folders:
+for folder in tqdm(out_folders):
     frame = pd.DataFrame(dtype=object)
     csv_name = "CSV" + os.sep + folder.replace(os.sep, "_") + ".csv"
 
